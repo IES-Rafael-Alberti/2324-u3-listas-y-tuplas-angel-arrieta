060e377eacdc35b78c7511212ea2d95a1730bc1a
Ejercicios_DAW_Python <aarrieta@iesrafaelalberti.es>
2023-10-31 10:03:28 +0100

    Configuración Tests Base

---
+++ test/test_6_1.py
@@ -0,0 +1,13 @@
+import pytest
+from src.Ej_6_1 import function
+
+
+@pytest.mark.parametrize(
+    "inMensaje, outMensaje",
+    [
+        ("Entrada1", "Salida1"),
+        ("Entrada2", "Salida2")
+    ]
+)
+def test_function(inMensaje, outMensaje):
+    assert function(inMensaje) == outMensaje
